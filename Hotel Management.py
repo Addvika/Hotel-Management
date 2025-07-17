f5d22e46367a17689ee8a93a28ecf959277be773
Addvika <117724792+Addvika@users.noreply.github.com>
2025-07-17 11:26:31 -0700

    Update Hotel Management.py

--- Hotel Management.py
+++ Hotel Management.py
@@ -4,38 +4,57 @@ import random
 def main():
     x="The Hotel"
     print("Welcome to ",x,"!")
-    res=input("Do you have a reservation?Y/N")
-    res="N"
-    n=int(input("How many people?"))
-    print("Would you like a:\n")
-    print("1.Standard")
-    print("2.Deluxe")
-    print("3.Suite")
-    room_type=int(input())
-    price=0
-    while not room_type.isdigit():
-        print("Try again.\n")
-        print("Would you like a:\n")
-        print("1.Standard")
-        print("2.Deluxe")
-        print("3.Suite")
-        room_type=int(input())
-    if root_type==1:
-        price+=65
-    elif room_type==2:
-        price+=105
-    else:
-        price+=35
-    print("The cost of 7 days is: ",price,"\n")
-    print("Thank you!Enjoy your stay!")
-    check_in=datetime.date()
-    check_out=check_in+timedelta(days=7)
-    c_id=random.randint(101,200)
-    print("Your Customer ID is: ",c_id)
-    to_eat=input("Would you like to eat something?Y/N")
-    if to_eat=="Y":
-        price+=food()
-
+    print("1.Check In")
+    print("2.Eat")
+    print("3.Check Out")
+    option=int(input())
+    while option:
+        if option>=1 and option<=3:
+            if option==1:
+                res=input("Do you have a reservation?Y/N")
+                res="N"
+                n=int(input("How many people?"))
+                print("Would you like a:\n")
+                print("1.Standard")
+                print("2.Deluxe")
+                print("3.Suite")
+                room_type=int(input())
+                price=0
+                while not room_type.isdigit():
+                    print("Try again.\n")
+                    print("Would you like a:\n")
+                    print("1.Standard")
+                    print("2.Deluxe")
+                    print("3.Suite")
+                    room_type=int(input())
+                    room_cost=0
+                if room_type==1:
+                    room_cost=65
+                elif room_type==2:
+                    room_cost=105
+                else:
+                    room_Cost=35
+                print("The cost of 7 days is: ",room_cost*7,"\n")
+                print("Thank you!Enjoy your stay!")
+                check_in=datetime.date()
+                check_out=check_in+timedelta(days=7)
+                c_id=random.randint(101,200)
+                print("Your Customer ID is: ",c_id)
+            elif option==2:
+                price+=food()
+            else:
+                print("You have ",check_out-check_in,"days to check out.Are you sure you want to check out?Y/N.")
+                to_check_out=input()
+                if to_check_out=="Y":
+                    check_out=datetime.date()
+                    room_cost*=(check_out-check_in+1)
+                    print("The room cost is:",price)
+                    print("The food bill is:", food_bill)
+                    print("Thank you for choosing ,",x,"!")
+                    exit()
+        
+    
+   
 def entrees_menu():
     print("1.Chicken Alfredo : $20")
     print("2.Cuban Chicken Sandwich : $18")
@@ -79,6 +98,7 @@ def food():
                 price+=food_cost[menu_number]
      to_order=input("Would you like to keep ordering?Y/N")
      print("Your food bill is: $",food_bill)
+     return food_bill
         
                     
                           
